330f02dff6692b2d6be14ce16c5a39016e53ee24
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-03-19 23:10:47 -0300

    alterando altura dos storys na tela desktop

--- src/components/home/story/NavBarStory.js
+++ src/components/home/story/NavBarStory.js
@@ -28,78 +28,78 @@ const Storys = () => {
         </button>
         <StyledBox id="scroll-container">
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=1" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=2" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=3" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=4" />
             </HistoryEvents>
           </div>       <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=5" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=6" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=7" />
             </HistoryEvents>
           </div>       <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=8" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=9" />
             </HistoryEvents>
           </div>       <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=10" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=11" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=12" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=13" />
             </HistoryEvents>
           </div>       <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=14" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=15" />
             </HistoryEvents>
           </div>
           <div>
-            <HistoryEvents>
+            <HistoryEvents className="historyEvents">
               <Img src="https://source.unsplash.com/random?=16" />
             </HistoryEvents>
           </div>
