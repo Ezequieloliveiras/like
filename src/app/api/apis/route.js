8e5801d29c5ab5176e8f7307b38976b14d78dcb6
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-03-23 22:51:54 -0300

    mudando url

--- src/app/api/apis/route.js
+++ src/app/api/apis/route.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export async function getUsers() {
   try {
-    const response = await axios.get('https://like-api-restfull.onrender.com/usuarios')
+    const response = await axios.get('https://like-api-restfull.onrender.com/api/usuarios')
     return response.data
   } catch (error) {
     console.error('Erro ao buscar usuários:', error)
@@ -14,7 +14,7 @@ export async function getUsers() {
 
 export async function deleteUser(id) {
   try {
-    await axios.delete(`https://like-api-restfull.onrender.com/usuarios/${id}`)
+    await axios.delete(`https://like-api-restfull.onrender.com/api/usuarios/${id}`)
     // Atualizar a lista de usuários após a exclusão
     
   } catch (error) {
@@ -24,7 +24,7 @@ export async function deleteUser(id) {
 
 export async function createComment(comment) {
   try {
-    await axios.post("https://like-api-restfull.onrender.com/usuarios", {comment})
+    await axios.post("https://like-api-restfull.onrender.com/api/usuarios", {comment})
     // Atualizar a lista de usuários após a exclusão
     
   } catch (error) {
